bcc7fbcdf96c7e0212af4f5f1c51cdac523d8e2c
qioixiy <qioixiy@gmail.com>
2015-06-10 00:21:43 +0800

    eval QEXPR builtin

--- lispx.c
+++ lispx.c
@@ -310,7 +310,104 @@ lval *builtin_op(lval *a, char *op) {
 	return x;
 }
 
+#define LASSERT(args, cond, err) \
+	if ( !(cond)) {lval_del(args); return lval_err(err);}
+
+lval *builtin_head(lval *a) {
+	/* check error conditions */
+	LASSERT(a, a->count == 1,
+			"Function 'head' passed too many arguments");
+	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+			"Function 'head' passed incorrect types!");
+	LASSERT(a, a->cell[0]->count != 0,
+			"Function 'head' passed {}!");
+
+	/* otherwise take first argument */
+	lval *v = lval_take(a, 0);
+
+	while(v->count > 1) {
+		lval_del(lval_pop(v, 1));
+	}
+
+	return v;
+}
+lval *builtin_tail(lval *a) {
+	/* check error conditions */
+	LASSERT(a, a->count == 1,
+			"Function 'head' passed too many arguments");
+	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+			"Function 'head' passed incorrect types!");
+	LASSERT(a, a->cell[0]->count != 0,
+			"Function 'head' passed {}!");
+
+	/* take first argument */
+	lval *v = lval_take(a, 0);
+	/* delete first element and return */
+	lval_del(lval_pop(v, 0));
+
+	return v;
+}
+
+lval *builtin_list(lval *a) {
+	a->type = LVAL_QEXPR;
+	return a;
+}
+
 lval *lval_eval(lval *v);
+lval *builtin_eval(lval *a) {
+	LASSERT(a, a->count == 1,
+			"Function 'head' passed too many arguments");
+	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+			"Function 'head' passed incorrect types!");
+	
+	lval *x = lval_take(a, 0);
+	x->type = LVAL_SEXPR;
+
+	return lval_eval(x);
+}
+
+lval *lval_join(lval *x, lval *y) {
+	/* for each cell in 'y' and it to 'x' */
+	while(y->count) {
+		x = lval_add(x, lval_pop(y, 0));
+	}
+
+	/* delete the empty 'y' and return 'x' */
+	lval_del(y);
+
+	return x;
+}
+
+lval *builtin_join(lval *a) {
+	for (int i = 0; i < a->count; i++) {
+		LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+				"Funtion 'join' passed incorrect type.");
+	}
+
+	lval *x = lval_pop(a, 0);
+	
+	while(a->count) {
+		x = lval_join(x, lval_pop(a, 0));
+	}
+
+	lval_del(a);
+
+	return x;
+}
+
+lval *builtin(lval *a, char *func) {
+	if(strcmp("list", func) == 0) {return builtin_list(a);}
+	if(strcmp("head", func) == 0) {return builtin_head(a);}
+	if(strcmp("tail", func) == 0) {return builtin_tail(a);}
+	if(strcmp("join", func) == 0) {return builtin_join(a);}
+	if(strcmp("eval", func) == 0) {return builtin_eval(a);}
+
+	if(strstr("+-*/%^", func)) {return builtin_op(a, func);}
+
+	lval_del(a);
+	return lval_err("Unknown Funtcion!");
+}
+
 lval *lval_eval_sexpr(lval *v) {
 	/* evaluate children */
 	for (int i = 0; i < v->count; i++) {
@@ -339,7 +436,7 @@ lval *lval_eval_sexpr(lval *v) {
 	}
 
 	/* call builtin with operator */
-	lval *result = builtin_op(v, f->sym);
+	lval *result = builtin(v, f->sym);
 	lval_del(f);
 	
 	return result;
@@ -366,12 +463,13 @@ int main(int argc, char **argv ){
 
 	mpca_lang(MPCA_LANG_DEFAULT,
 			  "\
-number   : /-?[0-9]+/ ;									\
-symbol   : '+' | '-' | '*' | '/' | '%' | '^' ;			\
-sexpr    : '(' <expr>* ')' ;							\
-qexpr    : '{' <expr>* '}' ;							\
-expr     : <number> | <symbol> | <sexpr> | <qexpr> ;	\
-lispx    : /^/ <expr>* /$/ ;							\
+number   : /-?[0-9]+/ ;											\
+symbol   : '+' | '-' | '*' | '/' | '%' | '^'					\
+		 | \"list\"| \"head\"| \"tail\"	| \"join\" | \"eval\";	\
+sexpr    : '(' <expr>* ')' ;									\
+qexpr    : '{' <expr>* '}' ;									\
+expr     : <number> | <symbol> | <sexpr> | <qexpr> ;			\
+lispx    : /^/ <expr>* /$/ ;									\
 ",
 			  Number, Symbol, Sexpr, Qexpr, Expr, Lispx);
 
